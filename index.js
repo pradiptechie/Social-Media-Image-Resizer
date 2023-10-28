a4f2bb6e7316a94f850e252e73274bd0e3dc410d
pradiptechie <kumarpradip3956@gmail.com>
2023-10-28 10:39:23 +0545

    done

--- index.js
+++ index.js
@@ -1,76 +1,69 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3400
 
 const bodyParser = require ('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
-
 app.use(express.static(__dirname)); // Serve static files, including uploaded images
 
-
 const multer = require('multer');
-// const upload = new multer ();
-// Configure Multer to use memory storage
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = new multer ();
 
+const sharp = require('sharp');
+const fs = require('fs');
 
 app.set('view engine', "hbs"); //setting view engine
 
 
 app.get('/', (req, res) => {
     res.render('main');
-    
 })
 
-const sharp = require('sharp');
-
 app.post('/', upload.single('mainImage'), (req, res) => {
-    //body-parser le body tag vitra ko elements lai direct access garn milx, like: req.body.element-Name
+
     const height = Number(req.body.height);
     const width = Number(req.body.width);
     console.log(`Target Height: ${height}`);
     console.log(`Target width: ${width}`);
-    
     // req.file contains the uploaded image as a buffer
-    const imageBuffer = req.file.buffer;
-    console.log(`imageBuffer: ${imageBuffer}`);
-  
+  const imageBuffer = req.file.buffer;
+  console.log(imageBuffer);
+
     sharp(imageBuffer)
     .metadata()
     .then(metadata => {
-      if (!metadata.width) {
+        if (!metadata.width) {
         throw new Error('Image width not found.');
-      }
-  
-      const originalWidth = metadata.width;
+        }
+
+        const originalWidth = metadata.width;
         console.log(`Original Width: ${originalWidth}`);
-      // Define aspect ratio and dimensions
-      const aspectRatio = width / height;
+        // Define aspect ratio and dimensions
+        const aspectRatio = width / height;
         console.log(`Aspect Ratio: ${aspectRatio}`);
-      
-      const targetHeight = Math.round(originalWidth / aspectRatio); // Calculate height
+        
+        const targetHeight = Math.round(originalWidth / aspectRatio); // Calculate height
         console.log(`Calculated Height: ${targetHeight}`);
+        console.log(imageBuffer);
+
         console.log(`===================================================`);
-      
-  
-      sharp(imageBuffer)
+
+        sharp(imageBuffer)
         .resize(originalWidth, targetHeight)
         .png()
         .toBuffer()
-        .then((processedImage) => {
-          const imageBase64 = processedImage.toString('base64');
-          const dataUrl = `data:image/png;base64,${imageBase64}`;
-          res.send(dataUrl);
-        // console.log(`Processed Image: ${dataUrl}`);
-
-          
-        })
-      .catch((err) => {
-        console.log(`Processing Error: ${err}`);
-        res.status(500).send('Image processing error');
-      });
+            .then((processedImage) => {
+                // Set the content type to display the image
+                res.set('Content-Type', 'image/png');
+                res.end(processedImage);
+            }).catch((error) => {
+                console.error('Error:', error);
+                res.status(500).send('Image processing error');
+            });
     });
-  });
+      
+  
+});
+
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
